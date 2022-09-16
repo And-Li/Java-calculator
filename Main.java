5e01dbc414c961be953c37761eb31bfb013b8c44
Andrey Lisitsa <75456617+And-Li@users.noreply.github.com>
2022-09-16 21:21:20 +0800

    some comments 4 future

--- Main.java
+++ Main.java
@@ -3,33 +3,45 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
 
-            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-            System.out.println("Enter the expression, e.g.: 3+5");
-            String newExp = myObj.nextLine();  // Read user input
-            System.out.println("You want to calculate: " + newExp);  // Output user input
-            char[] newExpArray = {newExp.charAt(0), newExp.charAt(1), newExp.charAt(2)};
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter the expression, e.g.: 3+5");
+        String newExp = myObj.nextLine();  // Read user input
+        char[] newExpArray = {newExp.charAt(0), newExp.charAt(1), newExp.charAt(2)};
 
-            int first = newExpArray[0] - 48; // Set variables for number-action-number in the input expression
-            String action = String.valueOf(newExpArray[1]);
-            int second = newExpArray[2] - 48;
+        //here needs to be a switch to transfer Roman input into arabic numbers
+        //here needs if array.length > 3 need to pop element 1 & 3
+        
+        int first = newExpArray[0] - 48; // Set variables for number-action-number in the input expression
+        String action = String.valueOf(newExpArray[1]);
+        int second = newExpArray[2] - 48; //Somehow the number needs to be reduced by 48; it works well, I'll leave it like that
+        System.out.println(newExpArray);
 
-            //int[] numberBorders = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Set borders for input numbers
-            //char[] actionBorders = {'-', '+', '/', '*'}; // Set borders for input arithmetic actions
-            //System.out.println(Arrays.toString(numberBorders));
-            //System.out.println(Arrays.toString(actionBorders));
-            //System.out.print(numberBorders[0]-numberBorders[1]);
-            //System.out.print(actionBorders[0]);
+
+
 
         switch (action) {
-            case "-" -> System.out.println(first - second);
-            case "+" -> System.out.println(first + second);
-            case "/" -> {
-                System.out.println((float) first / (float) second);
+            case "-" -> {
+                int result = first - second;
+                System.out.println(result);
+                break;
             }
-            case "*" -> System.out.println(first * second);
+            case "+" -> {
+                int result = first + second;
+                System.out.println(result);
+                break;
+            }
+            case "/" -> {
+                float result = (float) first / (float) second;
+                System.out.println(result);
+                break;
+            }
+            case "*" -> {
+                int result = first * second;
+                System.out.println(result);
+                break;
+            }
+
         }
 
 
-        }}
-
-
+    }}
