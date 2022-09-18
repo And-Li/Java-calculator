f210d8aacf69407474694eff37e7310a87d90615
Andrey Lisitsa <75456617+And-Li@users.noreply.github.com>
2022-09-18 14:14:29 +0800

    first working version
    splits input by " " into first/action/second
    transfers first and second from String to int 
    performs simple calculations with arabic numbers
    checks whether the input contains -+/*

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Enter the expression, e.g.: 3 + 5");
@@ -12,32 +11,50 @@ class Main {
         String action = userInputSplit[1];
         String second = userInputSplit[2];
 
-        if (first.contains("1")){System.out.println("it's an arabic number");}
+        
 
-        /*switch (action) {
+        //Should be done after Roman to arabic transfer:
+        int firstInt = Integer.parseInt(first); //transfers first into an integer
+        int secondInt = Integer.parseInt(second); // transfers second into an integer
+
+        //Detects if any of +-/* is in the user's input & throws exception if isn't:
+        if (action.contains("-")||
+                (action.contains("+"))||
+                (action.contains("/"))||
+                (action.contains("*")))
+        {System.out.print("");}
+        else {throw new Exception("No action signs detected");}
+
+        /*if (first.contains("1")){
+            int firstInt = Integer.parseInt(first);
+            System.out.println(firstInt);
+        }*/
+
+        // switch to perform actions +-/*
+        switch (action) {
             case "-" -> {
-                int result = first - second;
+                int result = firstInt - secondInt;
                 System.out.println(result);
                 break;
             }
             case "+" -> {
-                int result = first + second;
+                int result = firstInt + secondInt;
                 System.out.println(result);
                 break;
             }
             case "/" -> {
-                int result = first / second;
+                int result = firstInt / secondInt;
                 System.out.println(result);
                 break;
             }
             case "*" -> {
-                int result = first * second;
+                int result = firstInt * secondInt;
                 System.out.println(result);
                 break;
-            }*/ // switches actions +-/*
+            }
 
         //detects Roman numbers:
-        if (first.contains("i")||first.contains("v")||first.contains("x")||first.contains("l")){System.out.println("Roman 1 detected");}
-        if (second.contains("i")||second.contains("v")||second.contains("x")||second.contains("l")){System.out.println("Roman 2 detected");}
+        /*if (first.contains("i")||first.contains("v")||first.contains("x")||first.contains("l")){System.out.println("Roman 1 detected");}
+        if (second.contains("i")||second.contains("v")||second.contains("x")||second.contains("l")){System.out.println("Roman 2 detected");}*/
 
-    }}
+    }}}
