ed827d033fc389a48ae434e5ca5947e0da393fc8
Andrey Lisitsa <75456617+And-Li@users.noreply.github.com>
2022-09-19 09:19:49 +0800

    wrapped working model in needed method
    made it to be almost as nedded:
    public static int calc(String input), it works well if called.
    Need to make it return String and
    solve answer borders of both number types and roman-to-roman answers/

--- Main.java
+++ Main.java
@@ -1,29 +1,30 @@
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) throws Exception {
-
+    public static int calc(String input) throws Exception {
         Scanner scanner = new Scanner(System.in);  // Creates a Scanner object
         System.out.println("Enter the expression, e.g.: 3 + 5");
-        String userInput = scanner.nextLine();  // Reads user input
-        String [] userInputSplit = userInput.split(" "); // Splits input into number/action/number, in string format
+        input = scanner.nextLine();  // Reads user input
+        String [] userInputSplit = input.split(" "); // Splits input into number/action/number, in string format
         String first = userInputSplit[0]; // Initializes first number
         String action = userInputSplit[1];// Initializes the calculation sign
         String second = userInputSplit[2];// Initializes second number
+
         // These arrays are used for detecting if both input numbers are of one type (arabic or roman):
         String[] roman = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
         String[] arabic ={"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
 
         // Checks if input numbers are NOT floats:
-        if (first.contains(".")||(second.contains("."))){throw new Exception("No floats allowed!");}
-
+        if (first.contains(".")||(second.contains(".")))
+        {throw new Exception("No floats allowed!");
+        }
         // Checks if both input numbers are arabic or roman:
         if ((Arrays.asList(roman).contains(first)&&Arrays.asList(arabic).contains(second))||
                 ((Arrays.asList(arabic).contains(first))&&(Arrays.asList(roman).contains(second)))){
             throw new Exception("Both numbers should be either arabic or roman");
         }
-
         // Laziest way to make arabic numbers out of roman ones:
         switch (first){
             case "I" -> first = "1";
@@ -55,49 +56,39 @@ class Main {
         int secondInt = Integer.parseInt(second); //second into an integer
 
         // Checks if input numbers are in 1-10 range:
-        if (1 > firstInt || 1 > secondInt){throw new Exception("Too small");}
-        if (firstInt > 10 || secondInt > 10){throw new Exception("Too big");}
+        if (1 > firstInt || 1 > secondInt)
+        {throw new Exception("Too small");}
+        if (firstInt > 10 || secondInt > 10)
+        {throw new Exception("Too big");}
 
         // Detects if any of +-/* is in user's input or throws exception:
         if (action.contains("-")||
                 (action.contains("+"))||
                 (action.contains("/"))||
                 (action.contains("*")))
-        {System.out.print("");}
-        else {throw new Exception("No calculation sign detected");}
-
-        // A switch to perform all the calculations +-/*
-        switch (action) {
-            case "-" -> {
-                int result = firstInt - secondInt;
-                System.out.println(result);
-                break;
-            }
-            case "+" -> {
-                int result = firstInt + secondInt;
-                System.out.println(result);
-                break;
-            }
-            case "/" -> {
-                int result = firstInt / secondInt;
-                System.out.println(result);
-                break;
-            }
-            case "*" -> {
-                int result = firstInt * secondInt;
-                System.out.println(result);
-                break;
-            }
-
-        //detects Roman numbers:
-        /*if (first.contains("i")||first.contains("v")||first.contains("x")||first.contains("l")){System.out.println("Roman 1 detected");}
-        if (second.contains("i")||second.contains("v")||second.contains("x")||second.contains("l")){System.out.println("Roman 2 detected");}*/
-
-    }
-        //System.out.println(result);
+        {System.out.print("");
+        }
+        else {throw new Exception("No calculation sign detected");
+        }
+        int x = 0;
+        if (Objects.equals(action, "-")) {
+            x = (firstInt - secondInt);
+        } else if (Objects.equals(action, "+")) {
+            x = (firstInt + secondInt);
+        } else if (Objects.equals(action, "/")) {
+            x = (firstInt / secondInt);
+        } else if (Objects.equals(action, "*")) {
+            x = (firstInt * secondInt);
+        }
+        System.out.println(x);
+        return (x);
     }
 
+    public static void main(String[] args) throws Exception {
+        Scanner scannerNew = new Scanner(System.in);
+        calc(String.valueOf(scannerNew));
 
+    }
 
 }
 
